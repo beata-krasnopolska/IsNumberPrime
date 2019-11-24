332477abbe16369602270c2f2440fbd087765fdf
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-24 21:54:13 +0100

    Add project files.

---
+++ IsNumberPrime/PrimeFiner.cs
@@ -0,0 +1,29 @@
+﻿namespace IsNumberPrime
+{
+    class PrimeFiner
+    {
+        public bool FindPrime(int number)
+        {
+            if(number < 2)
+            {
+                return false;
+            }
+            if (number == 2)
+            {
+                return true;
+            }
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            for ( int i =3; i*i<=number; i += 2)
+            {
+                if(number%i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ IsNumberPrime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumberPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program takes a number and returns true if that number is a prime number. " +
+                "A Prime number is any positive integer that is evenly divisible by only two divisors: 1 and itself. " +
+                "The first 10 prime numbers are: 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.");
+
+            var findPrime = new PrimeFiner();
+            var prime_1 = findPrime.FindPrime(5);
+            var prime_2 = findPrime.FindPrime(1);
+            var prime_3 = findPrime.FindPrime(9);
+
+            Console.WriteLine(prime_1);
+            Console.WriteLine(prime_2);
+            Console.WriteLine(prime_3);
+
+            Console.ReadKey();
+        }
+    }
+}
